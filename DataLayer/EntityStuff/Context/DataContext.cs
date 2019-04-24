56a1bac855bc420171b958e336bb416c9fc8ddd3
Alexandr Shakavets <obscuralife@gmail.com>
2019-04-24 17:11:35 +0300

    Add project files.

---
+++ DataLayer/EntityStuff/Context/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using DataLayer.Model;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer.Context
+{
+    class DataContext : DbContext
+    {
+        DbSet<UserAccount> userAccounts { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ DataLayer/Model/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer.Model
+{
+    public class User
+    {
+        public string FirstName { get; private set; }
+        public string SeconName { get; private set; }
+
+        public User(string firstName, string secondName)
+        {
+            this.FirstName = firstName;
+            this.SeconName = secondName;
+        }
+    }
+}
